f3d3d88ac526ea1ceb6aba34f7554d6f7d35f51e
BobGioco <simekjakub47@gmail.com>
2023-02-03 17:02:12 +0100

    Version 2
    1. read file
    2. read word by word and censor it if needed.
    3. program will run till exit is inserted

--- main.go
+++ main.go
@@ -8,36 +8,42 @@ import (
 	"strings"
 )
 
-var file_name, word string
-
-func check_word(word string, banned_word string) bool {
-	if strings.ToLower(word) == banned_word {
-		return true
-	}
-	return false
-}
+var fileName, word string
 
 func main() {
-	flag := false
-	fmt.Scan(&file_name, &word)
-	file, err := os.Open(file_name)
+	char := "*"
+	fmt.Scan(&fileName)
+	file, err := os.Open(fileName)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-
 	scanner.Split(bufio.ScanWords)
+
+	var bannedWords []string
 	for scanner.Scan() {
-		if check_word(word, scanner.Text()) {
-			flag = true
-			break
-		}
+		bannedWords = append(bannedWords, scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(flag)
+
+	fmt.Scan(&word)
+	for word != "exit" {
+
+		for _, bannedWord := range bannedWords {
+			if strings.ToLower(word) == strings.ToLower(bannedWord) {
+				word = strings.Repeat(char, len(word))
+				break
+			}
+		}
+		fmt.Println(word)
+		fmt.Scan(&word)
+	}
+
+	fmt.Println("Bye!")
+
 }
